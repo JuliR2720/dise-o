74a65516c557f27334c70e5c6759566cf104b03d
Julian Ramos <juli12319@hotmail.com>
2022-02-01 11:44:30 +0100

    casi

--- script/jQuery_mas_media.js
+++ script/jQuery_mas_media.js
@@ -1,18 +1,12 @@
-﻿$(function () {
+﻿function update() {
+  $("h3").text(
+    "Viewport Size....." + $(window).width() + " x " + $(window).height()
+  );
+}
 
+update();
 
-    function update() {
-        $("h3").text("Viewport Size....." + $(window).width() + " x " + $(window).height() );
-    }
-
-    update();
-
-    /* Window event. Resize browser window */
-    $(window).resize(function () { update(); });
-
-
+/* Window event. Resize browser window */
+$(window).resize(function () {
+  update();
 });
-
-
-
-
